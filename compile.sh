7a4323360df3e136f5ceb9417b375f40b4ce95d3
Andrea Blasco <ablasco@fas.harvard.edu>
2017-04-04 16:15:04 -0700

    .

--- compile.sh
+++ compile.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
-cp .RData deck.Rmd func.R Slides/
-cd Slides
-crmd deck.Rmd
+output_dir=Paper
+
+# Slides
+if [ "$1" == "--slides" ]; then
+	cp .RData deck.Rmd func.R Slides/ && cd Slides && crmd deck.Rmd
+fi
+
+# Paper
+cp ~/Library/Application\ Support/BibDesk/library.bib $output_dir/
+cp _output.yml $output_dir
+cp *.Rmd $output_dir
+cp Template/* $output_dir/Template/
+cd $output_dir && crmd report.Rmd
+exit
+# echo "Copy data..."
+# cp $data $output_dir/.RData 
+# cp timing.csv $output_dir/
+# echo "Copy source R files..."
+# cp *.Rmd $output_dir/
+# echo "Copy config, templates, headers, footers..."
+# cp Templates/* $output_dir/Templates
+# cp Config/_output.yml $output_dir/
+# echo "Compile report..."
+# cd $output_dir && crmd report.Rmd > report.Rout 2> report.err
+# echo "Clean up..."
+# mv .RData *.Rmd Code/ 
+# echo "Done!"
+
+
+
+
 
